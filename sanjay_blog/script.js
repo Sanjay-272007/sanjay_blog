d6b86878295adfcdbe9cd19d78f18e6f857c883a
Sanjay-272007 <sanjays27032007@gmail.com>
2024-08-14 21:16:16 +0530

    Add files via upload

---
+++ sanjay_blog/script.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('open');
+}
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    const menuIcon = document.querySelector('.menu-icon');
+    
+    navLinks.classList.toggle('active');
+    menuIcon.classList.toggle('open');
+}
